e8344b6241c3774862a5d68148fa2eb0de96eed5
musumbidenis <musumbidenis@gmail.com>
2020-04-26 19:44:05 +0300

    Added  url launcher to my portfolio

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:splash_screen/splashScreen.dart';
 void main() {
   runApp(new MaterialApp(
     home: new SplashScreen(),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:splash_screen/homeScreen.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -9,7 +11,7 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> {
   startTime() async {
-    var _duration = new Duration(seconds: 2000);
+    var _duration = new Duration(seconds: 2);
     return new Timer(_duration, navigate);
   }
 
@@ -27,23 +29,35 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          height: 300,
-          width: 200,
-          child: Image(image: AssetImage('assets/01.png')
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 200,
+            child: Image(image: AssetImage('assets/01.png')),
+          ),
         ),
-        ),
-      ),
-      bottomSheet: Container(
-        height:50,
-        width:double.infinity,
-        child: Center(
-          child: Text("Developed by | musumbidenis",
-          style: TextStyle(color: Colors.grey, fontSize:15),
-          )
-        ),
-      )
-    );
+        bottomSheet: Container(
+          height: 50,
+          width: double.infinity,
+          child: Center(
+            child: RichText(
+              text: TextSpan(
+                  text: "Designed & Developed by | ",
+                  style: TextStyle(color: Colors.grey[600], fontSize: 12),
+                  children: <TextSpan>[
+                    TextSpan(
+                        text: "musumbidenis",
+                        style:
+                            TextStyle(color: Colors.blueAccent, fontSize: 12),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () async {
+                            if (await canLaunch("http://musumbidenis.co.ke")) {
+                              await launch("http://musumbidenis.co.ke");
+                            }
+                          })
+                  ]),
+            ),
+          ),
+        ));
   }
 }
