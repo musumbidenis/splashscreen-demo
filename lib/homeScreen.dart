376c0bf86e849a52e741e476612429eb81eb4624
musumbidenis <musumbidenis@gmail.com>
2020-04-26 18:27:47 +0300

    Completed the splash screen

---
+++ lib/homeScreen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('HomeScreen'),
+      ),
+      body: new Center(
+        child: new Text('Welcome to Home.!'),
+      ),
+    );
+  }
+}
--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -1,4 +1,6 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:splash_screen/homeScreen.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -6,10 +8,42 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  startTime() async {
+    var _duration = new Duration(seconds: 2000);
+    return new Timer(_duration, navigate);
+  }
+
+  void navigate() {
+    Navigator.push(
+        context, MaterialPageRoute(builder: (context) => HomePage()));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    startTime();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: Center(
+        child: Container(
+          height: 300,
+          width: 200,
+          child: Image(image: AssetImage('assets/01.png')
+        ),
+        ),
+      ),
+      bottomSheet: Container(
+        height:50,
+        width:double.infinity,
+        child: Center(
+          child: Text("Developed by | musumbidenis",
+          style: TextStyle(color: Colors.grey, fontSize:15),
+          )
+        ),
+      )
     );
   }
 }
